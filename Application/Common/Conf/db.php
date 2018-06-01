825d035e9bb64eea13de69a107fe44a60429ad76
youxianyen <youxianyen@sina.com>
2018-06-01 09:18:07 +0800

    first commit

---
+++ Application/Common/Conf/db.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/7/29
+ * Time: 16:54
+ */
+/**
+ * 数据库的配置信息
+ */
+return array(
+    'DB_TYPE' => 'mysqli',
+    'DB_HOST' => '127.0.0.1',
+    'DB_USER' => 'root',
+    'DB_PWD' => 'password',
+    'DB_PORT' => 3306,
+    'DB_NAME' => 'imooc_singcms',
+    'DB_CHARSET' => 'utf8',
+    'DB_PREFIX' =>'cms_',
+);
---
+++ Application/Home/Conf/config.php
@@ -0,0 +1,4 @@
+<?php
+return array(
+    'TMPL_ACTION_ERROR' => THINK_PATH . 'Tpl/dispatch_jump.tpl',
+);
